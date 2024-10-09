8382a750305e000b30acecf0da8166dde7480a95
Jefin Rojar <jefinrojar@gmail.com>
2024-10-09 15:28:54 +0530

    chatinterface added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return(
     <div>
       <Routes>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={<Login/>}/>
         <Route path='/chatinterface' element={<ChatInterface/>}/>
       </Routes>
     </div>
